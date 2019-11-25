1a15bb0a709d41818c8a89148c3aed476d869613
Muna Sharma <munasharma@pursuit.org>
2019-11-24 21:03:35 -0500

    Added react-table, added a takeview file, a data file and tableview css file

---
+++ src/data.js
@@ -0,0 +1,114 @@
+const data = [
+  {
+    server_id: "ea0977e7-67af-424f-b661-710dd2beb79e",
+    location: "Parsippany,NJ",
+    year: 2019,
+    month: "January 2019",
+    watt_hours: 100800,
+    pue: 1.65,
+    carbon_offset: "Positive",
+    carbon_emissions: 6
+  },
+  {
+    server_id: "ea0977e7-67af-424f-b661-710dd2beb79e",
+    location: "Sunnyvale,CA",
+    year: 2019,
+    month: "February 2019",
+    watt_hours: 104904,
+    pue: 1.75,
+    carbon_offset: "Positive",
+    carbon_emissions: 9
+  },
+  {
+    server_id: "ea0977e7-67af-424f-b661-710dd2beb79e",
+    location: "Dallas,TX",
+    year: 2019,
+    month: "March 2019",
+    watt_hours: 102960,
+    pue: 1.5,
+    carbon_offset: "Positive",
+    carbon_emissions: 5
+  },
+  {
+    server_id: "ea0977e7-67af-424f-b661-710dd2beb79e",
+    location: "Parsippany,NJ",
+    year: 2019,
+    month: "April 2019",
+    watt_hours: 110856,
+    pue: 1.65,
+    carbon_offset: "Positive",
+    carbon_emissions: 4
+  },
+  {
+    server_id: "ea0977e7-67af-424f-b661-710dd2beb79e",
+    location: "Dallas,TX",
+    year: 2019,
+    month: "May 2019",
+    watt_hours: 97920,
+    pue: 1.5,
+    carbon_offset: "Positive",
+    carbon_emissions: 6
+  },
+  {
+    server_id: "ea0977e7-67af-424f-b661-710dd2beb79e",
+    location: "Dallas,TX",
+    year: 2019,
+    month: "June 2019",
+    watt_hours: 105648,
+    pue: 1.5,
+    carbon_offset: "Positive",
+    carbon_emissions: 9
+  },
+  {
+    server_id: "ea0977e7-67af-424f-b661-710dd2beb79e",
+    location: "Sunnyvale,CA",
+    year: 2019,
+    month: "July 2019",
+    watt_hours: 104160,
+    pue: 1.75,
+    carbon_offset: "Positive",
+    carbon_emissions: 1
+  },
+  {
+    server_id: "ea0977e7-67af-424f-b661-710dd2beb79e",
+    location: "Sunnyvale,CA",
+    year: 2019,
+    month: "August 2019",
+    watt_hours: 105840,
+    pue: 1.75,
+    carbon_offset: "Positive",
+    carbon_emissions: 2
+  },
+  {
+    server_id: "ea0977e7-67af-424f-b661-710dd2beb79e",
+    location: "Sunnyvale,CA",
+    year: 2019,
+    month: "September 2019",
+    watt_hours: 102672,
+    pue: 1.75,
+    carbon_offset: "Positive",
+    carbon_emissions: 6
+  },
+  {
+    server_id: "ea0977e7-67af-424f-b661-710dd2beb79e",
+    location: "Parsippany,NJ",
+    year: 2019,
+    month: "October 2019",
+    watt_hours: 97920,
+    pue: 1.65,
+    carbon_offset: "Positive",
+    carbon_emissions: 3
+  },
+  {
+    server_id: "ea0977e7-67af-424f-b661-710dd2beb79e",
+    location: "Parsippany,NJ",
+    year: 2019,
+    month: "November 2019",
+    watt_hours: 48240,
+    pue: 1.65,
+    carbon_offset: "Positive",
+    carbon_emissions: 7
+  }
+];
+
+export default data;
