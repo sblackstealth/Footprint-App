1e322b43f9f850523356217e8728d54f035c20c1
Ceejaymar <martinezcj2@gmail.com>
2019-11-25 18:23:45 -0500

    Remove comments

--- src/containers/MainPage.js
+++ src/containers/MainPage.js
@@ -87,18 +87,6 @@ class MainPage extends Component {
             <div className="Main__did-you-know">
               <h3>Did you now?</h3>
               <CarbonFacts />
-              {/* <div className="Main__carbon-facts">
-                <p className="Main__fact">
-                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                </p>
-                <p className="Main__fact">
-                  Ad sed corrupti recusandae, ex facilis officia illum illo!
-                  Nulla ut impedit asperiores accusamus
-                </p>
-                <p className="Main__fact">
-                  odio eveniet, corporis totam dolore, quis sed autem?
-                </p>
-              </div> */}
             </div>
           </div>
         </div>
