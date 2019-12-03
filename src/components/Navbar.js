15d2d6314e7d9d7bc90c93730f77fb84a2c24049
Ceejaymar <martinezcj2@gmail.com>
2019-12-03 15:40:16 -0500

    Add navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import './navbar.css'
+
 const Navbar = () => {
   return (
     <nav className="Navbar">
-      <div className="Navbar__brand">FootPrints</div>
+      <h1 className="Navbar__brand">FootPrints</h1>
 
       <div>
-        <Link classname="Navbar__link" to="/">Home</Link>
-        <Link classname="Navbar__link" to="/about">About</Link>
+        <Link className="Navbar__link" to="/">Home</Link>
+        <Link className="Navbar__link" to="/about">About</Link>
       </div>
     </nav>
   );
